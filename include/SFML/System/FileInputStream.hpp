1df71a356e9a05a8377720bd9fb02c5b61b8307f
binary1248 <binary1248@hotmail.com>
2017-04-03 01:17:03 +0200

    Apply clang-tidy modernize suggestions.

--- include/SFML/System/FileInputStream.hpp
+++ include/SFML/System/FileInputStream.hpp
@@ -66,7 +66,7 @@ public:
     /// \brief Default destructor
     ///
     ////////////////////////////////////////////////////////////
-    virtual ~FileInputStream();
+    ~FileInputStream() override;
 
     ////////////////////////////////////////////////////////////
     /// \brief Open the stream from a file path
@@ -90,7 +90,7 @@ public:
     /// \return The number of bytes actually read, or -1 on error
     ///
     ////////////////////////////////////////////////////////////
-    virtual Int64 read(void* data, Int64 size);
+    Int64 read(void* data, Int64 size) override;
 
     ////////////////////////////////////////////////////////////
     /// \brief Change the current reading position
@@ -100,7 +100,7 @@ public:
     /// \return The position actually sought to, or -1 on error
     ///
     ////////////////////////////////////////////////////////////
-    virtual Int64 seek(Int64 position);
+    Int64 seek(Int64 position) override;
 
     ////////////////////////////////////////////////////////////
     /// \brief Get the current reading position in the stream
@@ -108,7 +108,7 @@ public:
     /// \return The current position, or -1 on error.
     ///
     ////////////////////////////////////////////////////////////
-    virtual Int64 tell();
+    Int64 tell() override;
 
     ////////////////////////////////////////////////////////////
     /// \brief Return the size of the stream
@@ -116,7 +116,7 @@ public:
     /// \return The total number of bytes available in the stream, or -1 on error
     ///
     ////////////////////////////////////////////////////////////
-    virtual Int64 getSize();
+    Int64 getSize() override;
 
 private:
 
